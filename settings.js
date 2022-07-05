ad815706523e9fb99b1595b90639bb4c641d9207
Alvinocaharles <54900965+alvino-prog@users.noreply.github.com>
2022-07-05 21:02:59 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,27 +19,27 @@ global.autobio = true
 //language
 //Available in indonesia & english
 //You can request others language 
-global.language = english  //change indonesia to english if you don't understand the language used by the bot
+global.language = indonesia  //change indonesia to english if you don't understand the language used by the bot
 
 
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
-global.github = "https://zeeone-ofc.github.io/" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
+global.botname = "C.C Bot-Md" //namabot kalian
+global.ownername= "Pino" //nama kalian
+global.myweb ="https://al-kun.xyz" //bebas asal jan hapus
+global.youtube = "https://youtube.com/c/XIllustriousMusicTv" //bebas asal jan hapus
+global.github = "https://github.com/alvino-prog/" //bebas
+global.email = "pino@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.owner = ["622150996855","622150996855"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
+global.owner = ["6281276242128"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "6281276242128" // nomor wa kalian
+global.ownernomerr = "+6281276242128" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.packname = 'C.C Bot' //sticker wm ubah
+global.author = 'Pino' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
